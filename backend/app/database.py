92451c828cd5a4f20630aac5f996d04b2cc41159
njoroofficial <mosesn579@gmail.com>
2026-01-05 14:31:06 +0300

    feat: enhance event management with environment variable support, improved data models, and initialization logic

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,25 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+# Construct the SQLAlchemy connection string
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
 
-supabase_url = "postgresql://postgres.wwjayfgmjugkzblcpkll:@Moses12960@@aws-1-eu-west-1.pooler.supabase.com:5432/postgres"
-engine = create_engine(supabase_url)
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL, poolclass=NullPool)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
